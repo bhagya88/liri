6a8a32d86c67f264dc9136297039fbd8152821f5
bhagya <bhagya@outlook.com>
2016-10-12 11:03:19 -0500

    random command function and logging functionality added

--- liri.js
+++ liri.js
@@ -1,69 +1,87 @@
+// include the modules
 var fs = require('fs');
 var request = require('request');
 var spotify = require('spotify');
 var Twitter = require('twitter');
+var readline = require('readline');
 var twitterKeys = require('./keys.js').twitterKeys;
 
-
+// gets tweets from account given
 function getMyTweets(){
-	 var client = new Twitter({
+
+	var screenName ='bhagya2016';
+	
+	// creates client object
+	var client = new Twitter({
 	  consumer_key: twitterKeys.consumer_key,
 	  consumer_secret: twitterKeys.consumer_secret,
 	  access_token_key: twitterKeys.access_token_key,
 	  access_token_secret: twitterKeys.access_token_secret 
 	});
 	 
-	var params = {screen_name: 'bhagya2016', count:2};
+	// parameters needed
+	var params = {screen_name:'bhagya2016', count:2};
 
 	client.get('statuses/user_timeline', params, function(error, tweets, response) {
 
-	  
+	  var output = '';
 	  if (!error) {
-	    tweets.forEach(function(tweet){
-	    	console.log("bhagya2016: " + tweet.text+" Posted on: "+tweet.created_at);
-	    });
 
+	    tweets.forEach(function(tweet){
+	    	output += screenName+': ' + tweet.text+" Posted on: "+tweet.created_at+'\n';
+	    });
+	    log(output);
 	  }
 	});
+
+	
 }
 
 
 function getSongInfo(){
 
-	arg += '&limit=1&offset=0';
+	
+	var queryString = arg + '&limit=1&offset=0';
 
-	spotify.search({ type: 'track', query: arg }, function(err, data) {
+	spotify.search({ type: 'track', query: queryString }, function(err, data) {
+	    var output = '';
 	    if ( err ) {
 	        return console.log('Error occurred: ' + err);
 	     }
 
-		console.log('Song Name: ',data.tracks.items[0].name);
-		console.log('Artists: ',data.tracks.items[0].artists[0].name);
-		console.log('Album: ',data.tracks.items[0].album.name);
-		console.log('Preview link: ',data.tracks.items[0].preview_url);
-
+		output += 'Song Name: '+data.tracks.items[0].name+'\n';
+		output += 'Artists: '+data.tracks.items[0].artists[0].name+'\n';
+		output += 'Album: '+data.tracks.items[0].album.name+'\n';
+		output += 'Preview link: '+data.tracks.items[0].preview_url+'\n';
+		log(output);
  	});
+
+	
 }
  
 
 function getMovieInfo(){
 
+	
 	request('http://www.omdbapi.com/?t='+arg+'&y=&plot=short&tomatoes=true&r=json','utf8',function(err,response,body){
+		var output = '';
 
-	if(err) {
-		return console.log('Error while getting data from omdb ' +err);
-	}
-    body = JSON.parse(body);
-    console.log("Title of the movie: ",body.Title);
-	console.log("Release Year : ",body.Year);
-	console.log("IMDB Rating : ",body.imdbRating);
-	console.log("Country of production: ",body.Country);
-	console.log("Language: ",body.Language);
-	console.log("Plot: ",body.Plot);
-	console.log("Actors: ",body.Actors);
-	console.log("Rotten Tomatoes Rating: ",body.tomatoRating);
-	console.log("Rotten Tomatoes URL: ",body.tomatoURL);
+		if(err) {
+			return console.log('Error while getting data from omdb ' +err);
+		}
+	    body = JSON.parse(body);
 
+	    output += 'Title of the movie: '+body.Title+'\n';
+		output += 'Release Year : '+body.Year+'\n';
+		output += 'IMDB Rating : '+body.imdbRating+'\n';
+		output += 'Country of production:'+body.Country+'\n';
+		output += 'Language: '+body.Language+'\n';
+		output += 'Plot: '+body.Plot+'\n';
+		output += 'Actors: '+body.Actors+'\n';
+		output += 'Rotten Tomatoes Rating: '+body.tomatoRating+'\n';
+		output += 'Rotten Tomatoes URL: '+body.tomatoURL+'\n';
+
+		log(output);
  	});
 
 }
@@ -79,18 +97,34 @@ function randomCommand(){
       liri();
   });
 
+  log('');
+
+}
+
+
+function log(output){
+	console.log(output); 
+	var str = '';
+
+	str += command+' '+arg.split('+').join(' ')+'\n';
+	str += output;
+	str += '---------------------------------------------'+'\n';
+	fs.appendFile('./log.txt',str, function(err,data){
+		if(err) return console.log(err);
+		})
 }
 
 
 
-
 var command = process.argv[2];
-var arg = process.argv.slice(3,process.argv.length).join('+');
+var arg = process.argv.slice(3,process.argv.length).join(' ');
 
 liri();
 
+
 function liri(){
 
+	
 	switch(command){
 
 		case 'myTweets':
@@ -110,5 +144,4 @@ function liri(){
 			  break;
 
 	}
-
 }
