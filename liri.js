1ddeadd06939ed3323f1b692b01b45045b9cbc73
bhagya <bhagya@outlook.com>
2016-10-12 12:22:07 -0500

    default functaionality added

--- liri.js
+++ liri.js
@@ -19,30 +19,34 @@ function getMyTweets(){
 	  access_token_secret: twitterKeys.access_token_secret 
 	});
 	 
-	// parameters needed
-	var params = {screen_name:'bhagya2016', count:2};
+	// parameters for twitter API
+	var params = {screen_name:'bhagya2016', count:20};
 
+	//API call
 	client.get('statuses/user_timeline', params, function(error, tweets, response) {
 
 	  var output = '';
-	  if (!error) {
-
+	  if (error) return console.log(error);
+	
 	    tweets.forEach(function(tweet){
 	    	output += screenName+': ' + tweet.text+" Posted on: "+tweet.created_at+'\n';
 	    });
-	    log(output);
-	  }
+
+	    log('myTweets',null,output);
+	  
 	});
 
 	
 }
 
 
-function getSongInfo(){
+function getSongInfo(songName){
+	var song;
+	// if song name not available set it to a default
+	(songName) ? song = songName : song = 'The Sign by Ace of Base';
+	var queryString = song + '&limit=1&offset=0';
 
-	
-	var queryString = arg + '&limit=1&offset=0';
-
+	//API call
 	spotify.search({ type: 'track', query: queryString }, function(err, data) {
 	    var output = '';
 	    if ( err ) {
@@ -53,17 +57,21 @@ function getSongInfo(){
 		output += 'Artists: '+data.tracks.items[0].artists[0].name+'\n';
 		output += 'Album: '+data.tracks.items[0].album.name+'\n';
 		output += 'Preview link: '+data.tracks.items[0].preview_url+'\n';
-		log(output);
- 	});
-
-	
+		
+		// call logging function
+		log('spotify-this-song',songName,output);
+	});
 }
  
 
-function getMovieInfo(){
+function getMovieInfo(movieName){
 
-	
-	request('http://www.omdbapi.com/?t='+arg+'&y=&plot=short&tomatoes=true&r=json','utf8',function(err,response,body){
+	var movie;
+	//if movie name not avaiable set it to a default
+	(movieName)? movie = movieName : movie= 'Mr. Nobody.';
+
+	//API call
+	request('http://www.omdbapi.com/?t='+movie+'&y=&plot=short&tomatoes=true&r=json','utf8',function(err,response,body){
 		var output = '';
 
 		if(err) {
@@ -81,49 +89,59 @@ function getMovieInfo(){
 		output += 'Rotten Tomatoes Rating: '+body.tomatoRating+'\n';
 		output += 'Rotten Tomatoes URL: '+body.tomatoURL+'\n';
 
-		log(output);
+		log('movie-this',movieName,output);
  	});
 
 }
 
+
+// takes the command given in random.txt and executes it
 function randomCommand(){
 
   fs.readFile('./random.txt','utf8', function(err, data){
 	  if (err) throw err;
 
 	  var tempArgArray = data.split(',');
-	  command = tempArgArray[0];
-	  arg = tempArgArray[1];
-      liri();
+	  var command = tempArgArray[0];
+	  var arg = tempArgArray[1];
+      liri(command, arg);
   });
 
-  log('');
+  log('do-what-it-says',null,'');
 
 }
 
-
-function log(output){
-	console.log(output); 
+// prints output to console and also log input and output
+function log(command,argument,output){
 	var str = '';
+	var arg;
 
-	str += command+' '+arg.split('+').join(' ')+'\n';
+	(argument) ? arg = argument: arg ='';
+	
+	console.log(output);
+	// str  has command, argument,also output
+	str += command+' '+arg+'\n';
 	str += output;
 	str += '---------------------------------------------'+'\n';
 	fs.appendFile('./log.txt',str, function(err,data){
 		if(err) return console.log(err);
-		})
+		});
 }
 
 
 
-var command = process.argv[2];
-var arg = process.argv.slice(3,process.argv.length).join(' ');
 
+
+// calling the main function
 liri();
 
+// main function which reads command and delegates to other functions
+function liri(cmd,argument){
 
-function liri(){
+	var command,arg;
 
+	(cmd) ? command = cmd : command = process.argv[2];
+	(argument) ? arg = argument:  arg= process.argv.slice(3,process.argv.length).join(' ');
 	
 	switch(command){
 
@@ -132,16 +150,19 @@ function liri(){
 			  break;
 
 		case 'spotify-this-song':
-			  getSongInfo();	
+			  getSongInfo(arg);	
 			  break;
 
 		case 'movie-this':
-			  getMovieInfo();
+			  getMovieInfo(arg);
 		 	  break;
 
 		case 'do-what-it-says':
 			  randomCommand();
 			  break;
-
+		default:
+			 var output = 'Error: No such command. Try movie-this, myTweets or spotify-this-song'+'\n';
+			 log(command,arg,output);
+			 break;
 	}
 }
